850c3a2e892c270a3ea844d5e6ea63cf3a5757dd
Anastasia Romanova <anastasiaromane@gmail.com>
2022-03-24 20:24:31 +0300

    Added a solution for questuion #3 in Section B

--- Case Study #2 - Pizza Runner/b-section-solution.sql
+++ Case Study #2 - Pizza Runner/b-section-solution.sql
@@ -17,7 +17,23 @@ GROUP BY
 ORDER BY
     a.runner_id
 
--- Is there any relationship between the number of pizzas and how long the order takes to prepare?
+-- 3. Is there any relationship between the number of pizzas and how long the 
+-- order takes to prepare?
+
+-- **** SOLUTION ****
+SELECT
+    a.order_id
+    , count(a.pizza_id)
+    , SUM(EXTRACT('minutes' FROM TO_TIMESTAMP(b.pickup_time,
+        'YYYY-MM-DD hh24:mi:ss') - a.order_time)) AS prep_time
+FROM pizza_runner.customer_orders  a
+    JOIN pizza_runner.runner_orders b ON (a.order_id = b.order_id)
+WHERE b.pickup_time <> 'null'
+GROUP BY
+    a.order_id
+ORDER BY
+    prep_time DESC
+
 -- What was the average distance travelled for each customer?
 -- What was the difference between the longest and shortest delivery times for all orders?
 -- What was the average speed for each runner for each delivery and do you notice any trend for these values?
