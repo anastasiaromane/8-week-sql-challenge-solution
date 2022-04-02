ded2d901316604754ccfff077a5114a4575ae8d2
Anastasia Romanova <anastasiaromane@gmail.com>
2022-04-02 14:25:17 +0300

    Added a solution for questuion #3 in Section C

--- Case Study #2 - Pizza Runner/c-section-solution.sql
+++ Case Study #2 - Pizza Runner/c-section-solution.sql
@@ -37,21 +37,47 @@ WITH extras AS (
     ranks AS (
     SELECT
         a.extra_id
-        , b.topping_name 
+        , b.topping_name
         , DENSE_RANK() OVER (ORDER BY COUNT(a.extra_id) DESC) AS rank
     FROM extras a
         JOIN pizza_runner.pizza_toppings b ON (a.extra_id = b.topping_id)
     GROUP BY
         a.extra_id
-        , b.topping_name 
+        , b.topping_name
     )
+
 SELECT
     extra_id
-    , topping_name 
+    , topping_name
 FROM ranks
 WHERE rank = 1
 
--- What was the most common exclusion?
+-- 3. What was the most common exclusion?
+WITH exclusions AS (
+    SELECT
+        CAST(UNNEST(STRING_TO_ARRAY(COALESCE(REPLACE(exclusions, 'null' , ''),
+            ''),',' )) AS INTEGER) AS exclusion_id
+    FROM pizza_runner.customer_orders
+  ),
+
+    ranks AS (
+    SELECT
+        a.exclusion_id
+        , b.topping_name
+        , DENSE_RANK() OVER (ORDER BY COUNT(a.exclusion_id) DESC) AS rank
+    FROM exclusions a
+        JOIN pizza_runner.pizza_toppings b ON (a.exclusion_id = b.topping_id)
+    GROUP BY
+        a.exclusion_id
+        , b.topping_name
+    )
+
+SELECT
+   exclusion_id
+   , topping_name
+FROM ranks
+WHERE rank = 1
+
 -- Generate an order item for each record in the customers_orders table in the format of one of the following:
 -- Meat Lovers
 -- Meat Lovers - Exclude Beef
